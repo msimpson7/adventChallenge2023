5777070473a10f04cb837d964e135f9c35cf3847
Marcus Simpson <msimpson7@gmail.com>
2024-02-06 08:48:11 -0600

    Day 1 Part 2 Complete

--- day1_trebuchet/trebuchet_part2.py
+++ day1_trebuchet/trebuchet_part2.py
@@ -1,40 +1,20 @@
 import re
 
-input_file = 'test.txt'
-
-
-def change_to_num(num_char):
-    if len(num_char) == 1:
-        return num_char
-    elif num_char == "one":
-        return "1"
-    elif num_char == "two":
-        return "2"
-    elif num_char == "three":
-        return "3"
-    elif num_char == "four":
-        return "4"
-    elif num_char == "five":
-        return "5"
-    elif num_char == "six":
-        return "6"
-    elif num_char == "seven":
-        return "7"
-    elif num_char == "eight":
-        return "8"
-    elif num_char == "nine":
-        return "9"
-    else:
-        return "Not a number"
+input_file = 'puzzle_input.txt'
 
 
 def get_numbers(line):
-    pattern = "(\\d)?(one)?(two)?(three)?(four)?(five)?(six)?(seven)?(eight)?(nine)?"
-    pattern_reverse = "(\\d)?(eno)?(owt)?(eerht)?(ruof)?(evif)?(xis)?(neves)?(thgie)?(enin)?"
-    first_num = re.search(pattern, line)[0]
-    last_num = re.search(pattern_reverse, line[::-1].replace("\n", ""))[0]
-    print(f"first_num is '{first_num}', last_num is '{last_num}'")
-    return int(change_to_num(first_num) + change_to_num(last_num[::-1]))
+    first_num = re.search("\\d", line)[0]
+    last_num = re.search('\\d', line[::-1])[0]
+    return int(first_num + last_num)
+
+
+def clean_line(line):
+    updated_line = (line.replace('one', 'o1e').replace('two', 't2o').replace('three', 't3e')
+                    .replace('four', 'f4r').replace('five', 'f5e').replace('six', 's6x')
+                    .replace('seven', 's7n').replace('eight', 'e8t').replace('nine', 'n9e'))
+
+    return updated_line
 
 
 try:
@@ -45,6 +25,7 @@ except FileNotFoundError as e:
 
 summation = 0
 for line in file_info:
-    number = get_numbers(line)
+    new_line = clean_line(line)
+    number = get_numbers(new_line)
     summation += number
 print(f'The sum of the numbers is: {summation}')
